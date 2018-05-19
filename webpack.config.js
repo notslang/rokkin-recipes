8c3e793d5011029154c6ec125305816cf92b314f
Sean Lang <slang800@gmail.com>
2018-05-19 17:08:32 -0500

    setup webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+var path = require('path')
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'public')
+  },
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ],
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true
+  }
+}
