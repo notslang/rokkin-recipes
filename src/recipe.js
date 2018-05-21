a8e35867446ffb91c19b9916fc1c997be75d644f
Sean Lang <slang800@gmail.com>
2018-05-21 02:36:25 -0500

    add support for markdown parsing

--- src/recipe.js
+++ src/recipe.js
@@ -1,13 +1,11 @@
 'use strict'
 import React from 'react'
 import {Link} from 'react-router-dom'
+import marked from 'marked'
 
 const Recipe = ({recipe}) => {
   var ingredients = recipe.ingredients.map((ingredient, i) => (
     <li key={ingredient.id}>{ingredient.name}</li>
-  ))
-  var instructions = recipe.instructions.split('\n').map((step, i) => (
-    <p key={i}>{step}</p>
   ))
   return (
     <div id='recipe'>
@@ -15,13 +13,13 @@ const Recipe = ({recipe}) => {
       <h1>{recipe.name}</h1>
       <p>Recipe added {new Date(recipe.timeAdded * 1000).toLocaleString()}</p>
       <h2>Description</h2>
-      <p>{recipe.description}</p>
+      <div dangerouslySetInnerHTML={{__html: marked(recipe.description)}} />
       <h2>Ingredients</h2>
       <ul>
         {ingredients}
       </ul>
       <h2>Instructions</h2>
-      {instructions}
+      <div dangerouslySetInnerHTML={{__html: marked(recipe.instructions)}} />
     </div>
   )
 }
