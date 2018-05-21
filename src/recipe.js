b3463abb1eaedec555a94312abb1265e7a8bf05c
Sean Lang <slang800@gmail.com>
2018-05-20 20:52:27 -0500

    change recipe into functional component

--- src/recipe.js
+++ src/recipe.js
@@ -1,32 +1,29 @@
 'use strict'
-import React, {Component} from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 
-class Recipe extends Component {
-  render () {
-    var {recipe} = this.props
-    var ingredients = recipe.ingredients.map((ingredient, i) => (
-      <li key={i}>{ingredient.name}</li>
-    ))
-    var instructions = recipe.instructions.split('\n').map((step, i) => (
-      <p key={i}>{step}</p>
-    ))
-    return (
-      <div className='recipe'>
-        <Link to={'/edit/' + recipe.id}>edit</Link>
-        <h1>{recipe.name}</h1>
-        <p>Recipe added {new Date(recipe.timeAdded * 1000).toLocaleString()}</p>
-        <h2>Description</h2>
-        <p>{recipe.description}</p>
-        <h2>Ingredients</h2>
-        <ul>
-          {ingredients}
-        </ul>
-        <h2>Instructions</h2>
-        {instructions}
-      </div>
-    )
-  }
+const Recipe = ({recipe}) => {
+  var ingredients = recipe.ingredients.map((ingredient, i) => (
+    <li key={i}>{ingredient.name}</li>
+  ))
+  var instructions = recipe.instructions.split('\n').map((step, i) => (
+    <p key={i}>{step}</p>
+  ))
+  return (
+    <div className='recipe'>
+      <Link to={'/edit/' + recipe.id}>edit</Link>
+      <h1>{recipe.name}</h1>
+      <p>Recipe added {new Date(recipe.timeAdded * 1000).toLocaleString()}</p>
+      <h2>Description</h2>
+      <p>{recipe.description}</p>
+      <h2>Ingredients</h2>
+      <ul>
+        {ingredients}
+      </ul>
+      <h2>Instructions</h2>
+      {instructions}
+    </div>
+  )
 }
 
 export default Recipe
