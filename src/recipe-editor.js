c513e921825ea0156ad6e5b0d4dbb532dce1176d
Sean Lang <slang800@gmail.com>
2018-05-20 23:41:55 -0500

    add editor for ingredients

--- src/recipe-editor.js
+++ src/recipe-editor.js
@@ -1,18 +1,47 @@
 'use strict'
 import React, {Component} from 'react'
-import cloneDeep from 'lodash.clonedeep'
+import clone from 'lodash.clone'
 import {Link} from 'react-router-dom'
 
 class RecipeEditor extends Component {
   handleChange (property, event) {
     var {model, recipe} = this.props
-    var newRecipe = cloneDeep(recipe)
+
+    // we can use clone here becuase our ingredient editing is separate
+    var newRecipe = clone(recipe)
     newRecipe[property] = event.target.value
     model.updateRecipe(newRecipe)
   }
 
+  addIngredient () {
+    var {model, recipe} = this.props
+    model.addIngredient(recipe.id, 'ingredient name...')
+  }
+
+  handleIngredientChange (ingredientId, event) {
+    var {model, recipe} = this.props
+    model.updateIngredient(recipe.id, {
+      id: ingredientId,
+      name: event.target.value
+    })
+  }
+
+  deleteIngredient (ingredientId, event) {
+    var {model, recipe} = this.props
+    model.deleteIngredient(recipe.id, ingredientId)
+  }
+
   render () {
     var {recipe} = this.props
+    var ingredients = recipe.ingredients.map((ingredient, i) => {
+      return (
+        <label key={ingredient.id}>
+          Name:
+          <input type='text' value={ingredient.name} onChange={this.handleIngredientChange.bind(this, ingredient.id)} />
+          <input type='button' onClick={this.deleteIngredient.bind(this, ingredient.id)} value='delete' />
+        </label>
+      )
+    })
     return (
       <div id='recipe-editor'>
         <Link to={'/' + recipe.id}>close editor</Link>
@@ -24,6 +53,8 @@ class RecipeEditor extends Component {
           <label htmlFor='description'>Description:</label>
           <textarea id='description' value={recipe.description} onChange={this.handleChange.bind(this, 'description')} />
           <label htmlFor='ingredients'>Ingredients:</label>
+          {ingredients}
+          <input type='button' onClick={this.addIngredient.bind(this)} value='add ingredient' />
           <label htmlFor='instructions'>Instructions:</label>
           <textarea id='instructions' value={recipe.instructions} onChange={this.handleChange.bind(this, 'instructions')} />
         </form>
